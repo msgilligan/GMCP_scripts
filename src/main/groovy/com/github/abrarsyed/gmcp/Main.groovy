9e66b6bf3b3127fe325cc1fe05b104b950d4c53c
AbrarSyed <sacabrarsyed@gmail.com>
2013-05-04 16:35:20 -0500

    fixed script, started CSV reading

--- src/main/groovy/com/github/abrarsyed/gmcp/Main.groovy
+++ src/main/groovy/com/github/abrarsyed/gmcp/Main.groovy
@@ -7,27 +7,27 @@ class Main
 	public static final File tmp = new File("tmp");
 	public static final File resources = new File("resources");
 	public static final File logs = new File(tmp, "logs");
-	
+
 	public static void main(args)
 	{
 		logs.mkdirs()
 		tmp.mkdirs()
 		resources.mkdirs()
-		
+
 		downloadStuff()
 
 		println "DeObfuscating With SpecialSource !!!!!!!!!!!!"
 
 		deobfuscate()
-		
+
 		println "Applying Exceptor (MCInjector) !!!!!!!!!!!!"
-		
+
 		inject()
-		
+
 		println "UNZIPPING !!!!!!!!!!!!"
-		
+
 		Util.unzip(new File(tmp, "Minecraft_exc.jar"), new File(tmp, "extracted"), false)
-		
+
 		println "DECOMPILING !!!!!!!!!!!!"
 
 		decompile()
@@ -53,18 +53,22 @@ class Main
 		//JarBouncer.retroGuardDeObf(cp.toString(), resources.path+"/srgs/client_rg.cfg");
 		JarBouncer.specialSource(new File(tmp, "jars/Minecraft.jar"), new File(tmp, "Minecraft_ss.jar"), new File(resources, "srgs/client.srg"));
 	}
-	
+
 	def static inject()
 	{
 		JarBouncer.injector(new File(tmp, "Minecraft_ss.jar"), new File(tmp, "Minecraft_exc.jar"), new File(resources, "joined.exc"))
 	}
-	
-	def static generateSRGs()
+
+	def static readCSV()
 	{
 		def reader = new CSVReader();
 		def elements = reader.readAll();
-		
-		
+
+		elements.each
+		{
+			it.each
+			{ println it }
+		}
 	}
 
 	def static downloadStuff()
