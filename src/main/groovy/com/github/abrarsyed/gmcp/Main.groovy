ca46799c41a2792d543c1dfc7f8e2f4aeca732dd
AbrarSyed <sacabrarsyed@gmail.com>
2013-06-22 18:44:34 -0500

    attempted other diff-util lib

--- src/main/groovy/com/github/abrarsyed/gmcp/Main.groovy
+++ src/main/groovy/com/github/abrarsyed/gmcp/Main.groovy
@@ -4,6 +4,7 @@ import java.util.zip.ZipEntry
 import java.util.zip.ZipFile
 import java.util.zip.ZipOutputStream
 
+import name.fraser.neil.plaintext.DiffMatchPatch
 import net.md_5.specialsource.AccessMap
 import net.md_5.specialsource.Jar
 import net.md_5.specialsource.JarMapping
@@ -29,57 +30,63 @@ class Main
 	public static String MC_VERSION = "1.5.2"
 	public static String FORGE_VERSION = "7.8.0.710"
 
+	public static DiffMatchPatch patcher = new DiffMatchPatch()
+
 	public static void main(args)
 	{
 		os = Util.getOS()
+		patcher.Match_Distance = 0000
+		patcher.Match_Threshold = 1
+		patcher.Patch_DeleteThreshold = 1
+		patcher.Patch_Margin = 100
 
-//		println "cleaning up past builds...."
-//		Util.createOrCleanDir(Constants.DIR_TEMP)
-//		Util.createOrCleanDir(Constants.DIR_LOGS)
-//
-//		println "DOWNLOADING STUFF !!!!!!!!!!!!"
-//
-//		downloadStuff()
-//
-//		println "MERGING JARS !!!!!!!!!!!!"
-//
-//		mergeJars(Constants.JAR_CLIENT, Constants.JAR_SERVER)
-//
-//		println "FIXING PACKAGES!!!!!!!!"
-//
-//		// creates the package fixer
-//		(new PackageFixer(new File(Constants.DIR_MAPPINGS, Constants.CSVS["packages"]))).with {
-//			// calls the following on the package fixer.
-//			// gotta love groovy :)
-//			fixSRG(new File(Constants.DIR_MAPPINGS, "joined.srg"), new File(Constants.DIR_MAPPINGS, "packaged.srg"))
-//			fixExceptor(new File(Constants.DIR_MAPPINGS, "joined.exc"), new File(Constants.DIR_MAPPINGS, "packaged.exc"))
-//			fixPatch(new File(Constants.DIR_MCP_PATCHES, "minecraft_ff.patch"))
-//			fixPatch(new File(Constants.DIR_MCP_PATCHES, "minecraft_server_ff.patch"))
-//		}
-//
-//		println "DeObfuscating With SpecialSource !!!!!!!!!!!!"
-//
-//		deobfuscate(Constants.JAR_MERGED, Constants.JAR_DEOBF)
-//
-//		println "Applying Exceptor (MCInjector) !!!!!!!!!!!!"
-//
-//		inject()
-//
-//		println "UNZIPPING !!!!!!!!!!!!"
-//
-//		Util.unzip(Constants.JAR_EXCEPTOR, Constants.DIR_EXTRACTED, false)
-//
-//		println "COPYING CLASSES!!!!!!!"
-//
-//		copyClasses(Constants.DIR_EXTRACTED, Constants.DIR_CLASSES)
-//
-//		println "DECOMPILING !!!!!!!!!!!!"
-//
-//		decompile()
-//
-//		println "APPLY FF FIXES!!!!!!!"
-//
-//		FFPatcher.processDir(Constants.DIR_SOURCES)
+		println "cleaning up past builds...."
+		Util.createOrCleanDir(Constants.DIR_TEMP)
+		Util.createOrCleanDir(Constants.DIR_LOGS)
+
+		println "DOWNLOADING STUFF !!!!!!!!!!!!"
+
+		downloadStuff()
+
+		println "MERGING JARS !!!!!!!!!!!!"
+
+		mergeJars(Constants.JAR_CLIENT, Constants.JAR_SERVER)
+
+		println "FIXING PACKAGES!!!!!!!!"
+
+		// creates the package fixer
+		(new PackageFixer(new File(Constants.DIR_MAPPINGS, Constants.CSVS["packages"]))).with {
+			// calls the following on the package fixer.
+			// gotta love groovy :)
+			fixSRG(new File(Constants.DIR_MAPPINGS, "joined.srg"), new File(Constants.DIR_MAPPINGS, "packaged.srg"))
+			fixExceptor(new File(Constants.DIR_MAPPINGS, "joined.exc"), new File(Constants.DIR_MAPPINGS, "packaged.exc"))
+			fixPatch(new File(Constants.DIR_MCP_PATCHES, "minecraft_ff.patch"))
+			fixPatch(new File(Constants.DIR_MCP_PATCHES, "minecraft_server_ff.patch"))
+		}
+
+		println "DeObfuscating With SpecialSource !!!!!!!!!!!!"
+
+		deobfuscate(Constants.JAR_MERGED, Constants.JAR_DEOBF)
+
+		println "Applying Exceptor (MCInjector) !!!!!!!!!!!!"
+
+		inject()
+
+		println "UNZIPPING !!!!!!!!!!!!"
+
+		Util.unzip(Constants.JAR_EXCEPTOR, Constants.DIR_EXTRACTED, false)
+
+		println "COPYING CLASSES!!!!!!!"
+
+		copyClasses(Constants.DIR_EXTRACTED, Constants.DIR_CLASSES)
+
+		println "DECOMPILING !!!!!!!!!!!!"
+
+		decompile()
+
+		println "APPLY FF FIXES!!!!!!!"
+
+		FFPatcher.processDir(Constants.DIR_SOURCES)
 
 		println "APPLYING MCP PATCHES!!!!!!!"
 
@@ -205,7 +212,8 @@ class Main
 		def rawPatch = new File(Constants.DIR_MCP_PATCHES, "minecraft_ff.patch").text.readLines()
 
 		def patchMap = [:]
-		def patternDiff = /diff.*?minecraft\\(.+?) .*?/
+		def patternDiff = /^diff.*?minecraft\\(.+?) .*?/
+		def startPattern = /^[+]{3}.*/
 
 		def currentFile, startIndex = 0, endIndex = 0
 		rawPatch.eachWithIndex
@@ -217,23 +225,26 @@ class Main
 				if (currentFile == null)
 				{
 					currentFile = matcher[0][1]
-					startIndex = i+1
+					//startIndex = i+1
 				}
 
 				endIndex = i-1
 				if (endIndex > 0)
 				{
 					def patchlist = rawPatch.subList(startIndex, endIndex)
-					patchMap[currentFile] = DiffUtils.parseUnifiedDiff(patchlist)
+					//patchMap[currentFile] = DiffUtils.parseUnifiedDiff(patchlist)
+					patchMap[currentFile] = patcher.patch_fromText(patchlist.join("\n"))
 
 					// for next one
 					currentFile = matcher[0][1]
-					startIndex = i+1
+					//startIndex = i+1
 					endIndex = 0
 				}
 				return
 			}
-				
+
+			if (obj ==~ startPattern)
+				startIndex = i+1
 		}
 
 		print ""
@@ -241,20 +252,21 @@ class Main
 		def counter = 0, success = 0
 		patchMap.each
 		{ file, delta ->
-			try
-			{
-				file = new File(Constants.DIR_SOURCES, file)
-				def lines = file.text.readLines()
-				lines = delta.applyTo(lines)
-				file.write(lines.join("\n"))
+			file = new File(Constants.DIR_SOURCES, file)
+			//def lines = file.text.readLines()
+			def result = patcher.patch_apply(delta, file.text)
+			def newText = result[0]
+			def failed = false
+			result[1].each {
+				println "$file >> "+it
+				if (!it)
+					failed = true
+			}
+			//lines = delta.applyTo(lines)
+			//file.write(lines.join("\n"))
+			file.write(newText)
+			if (!failed)
 				success++
-			}
-			catch(Exception e)
-			{
-				println "error patching "+file+"   skipping."
-				if (counter <= 1)
-					e.printStackTrace();
-			}
 			counter++
 		}
 
@@ -291,7 +303,7 @@ class Main
 			{
 				println "error patching "+file+"   skipping."
 				if (counter <= 1)
-					e.printStackTrace();
+					e.printStackTrace()
 			}
 			counter++
 		}
